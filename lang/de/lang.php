8c0eefbf6d5e19d79695a3709a762013e35a10af
Gina Haeussge <gina@foosel.net>
2009-11-02 23:13:08 +0100

    Added german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * additional language strings used by plugin
+ *
+ * german version
+ *
+ * @author Gina Haeussge <osd@foosel.net>
+ */
+
+$lang['filename'] = 'Dateiname';
+$lang['filesize'] = 'Dateigröße';
+$lang['lastmodified'] = 'Letzte Änderung';
+$lang['error_nomatch'] = 'Keine Treffer';
+$lang['error_outsidejail'] = 'Zugriff verweigert';
